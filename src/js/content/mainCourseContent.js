9dfa447a282f031bb1a9c03bf2f8316a7b7ffc67
awedzonka <aneta.wedzonka@gmail.com>
2022-07-09 16:59:36 +0200

    slajder part 4- styling

--- src/js/content/mainCourseContent.js
+++ src/js/content/mainCourseContent.js
@@ -7,14 +7,20 @@ const MainCourseContent = () => {
         if (slideNumber >= 37) {
             return
         }
-        setSlideNumber(prevState => prevState + 1)
+        setSlideNumber(prevState => {
+            return parseInt(prevState) + 1
+        })
+
+        document.querySelector("#slideNumber").value = parseInt(slideNumber) + 1;
     }
 
     const changePreviousSlide = () => {
         if (slideNumber <= 1) {
             return
         }
-        setSlideNumber(prevState => prevState - 1)
+        setSlideNumber(prevState => parseInt(prevState) - 1)
+
+        document.querySelector("#slideNumber").value = parseInt(slideNumber) -1;
     }
 
     const chooseExactSlide = (e) => {
@@ -33,13 +39,12 @@ const MainCourseContent = () => {
     return (
         <>
             <div className="mainContent">
-                {/*<input type="number" name="number" defaultValue={slideNumber} min="1" max="37" id="slideNumber" onChange={getSlideNumber} /> /37*/}
                 <input type="number" name="number" defaultValue="1" min="1" max="37" id="slideNumber"/> /37
                 <button type="submit" onClick={chooseExactSlide}> wczytaj slajd</button>
                 <h2> Slajd nr {slideNumber} </h2>
                 <button onClick={changePreviousSlide}>poprzedni slajd</button>
                 <button onClick={changeNextSlide}>następny slajd</button>
-                <div>
+                <div className="slaidView">
                     <img src={`../../images/slides/Slajd${slideNumber}.PNG`}/>
                 </div>
             </div>
