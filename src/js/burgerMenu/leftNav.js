5bae3f190bcc8377f247e66a3931745501c95504
awedzonka <aneta.wedzonka@gmail.com>
2022-07-09 20:53:11 +0200

    improve navigation

--- src/js/burgerMenu/leftNav.js
+++ src/js/burgerMenu/leftNav.js
@@ -2,42 +2,45 @@ import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
 
-const Ul = styled.ul`
-  @media (max-width: 1200px) {
-    transform: ${({open}) => open ? `translateX()` : `translateX(100%)`};
-    transition: transform 0.3s linear;
-`
+// const Ul = styled.ul`
+//   @media (max-width: 1200px) {
+//     transform: ${({open}) => open ? `translateX()` : `translateX(100%)`};
+//     transition: transform 0.3s linear;
+// `
 
 const LeftNavigation = ({open}) => {
-    // const [isHover, setIsHover] = useState({open})
-    const [stateDisplay, setStateDisplay] = useState("none")
-
-    useEffect(() => {
-        if (open) {
-            setStateDisplay("block")
-        } else {
-            setStateDisplay("none")
-        }
-
-        if (window.innerHeight > 1200) {
-            setStateDisplay("block")
-        }
-
-
-    }, [open]);
-
-    window.addEventListener('resize', function (e) {
-        if (window.innerWidth > 1200) {
-            setStateDisplay("flex")
-        } else {
-            setStateDisplay("none")
-        }
-        console.log()
-    });
+    const [isHover, setIsHover] = useState({open})
+    // const [stateDisplay, setStateDisplay] = useState("none")
+    //
+    // useEffect(() => {
+    //     if (open) {
+    //         setStateDisplay("block")
+    //     } else {
+    //         setStateDisplay("none")
+    //     }
+    //
+    //     if (window.innerHeight > 1200) {
+    //         setStateDisplay("block")
+    //     }
+    //
+    //
+    // }, [open]);
+    //
+    // window.addEventListener('resize', function (e) {
+    //     if (window.innerWidth > 1200) {
+    //         setStateDisplay("flex")
+    //     } else {
+    //         setStateDisplay("none")
+    //     }
+    //     console.log()
+    // });
     return (
         <>
-
-            <ul open={open} className="homeNav" style={{display: stateDisplay}}>
+            <ul
+                // open={open}
+                className="homeNav"
+                // style={{display: stateDisplay}}
+            >
                 <li><Link to="/">strona główna</Link></li>
                 <li><Link to="/course"> materiały szkoleniowe </Link></li>
                 <li><Link to="/registration"> rejestracja </Link></li>
